31d671ae2873ab8dd4ed7808aa2040c04ab37dc5
Jaiskid <neeraj001jaiswal@gmail.com>
2020-03-12 00:21:07 +0530

    codeupdate

---
+++ Two_sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> twosum(vector<int>&nums,int target){
+    unordered_map<int,int> lookup;
+    for(int i=0;i<nums.size();++i){
+        if(lookup.count(target-nums[i])){
+            return {lookup[target-nums[i]],i};
+        }
+        lookup[nums[i]]=i;
+    }
+}
+int main() {
+   int n;
+   cin>>n;
+   vector<int> nums;
+
+   while(n--){
+       int data;
+       cin>>data;
+       nums.push_back(data);
+
+   }
+   int target;
+   cin>>target;
+   vector<int> result;
+   result=twosum(nums,target);
+
+   for(int i=0;i<result.size();++i){
+       cout<<result[i]<<" ";
+   }
+}
